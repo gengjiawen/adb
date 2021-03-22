3512944d508e5f798b7d0d3aa3ccc43bd70cd8a2
Woody Lin <woodylin@google.com>
2021-03-22 03:04:21 +0000

    Revert "Openscreen discovery integration for adb client."
    This reverts commit f4ba8d73079b99532069dbe888a58167b8723d6c.
    
    Reason for revert: Breaking sdk_mac build
    Bug: 183340309
    
    Change-Id: I4d5b9c41c3c20756d0b675c1e0f0ec018054819c

--- client/main.cpp
+++ client/main.cpp
@@ -34,7 +34,6 @@
 #include "adb_auth.h"
 #include "adb_client.h"
 #include "adb_listeners.h"
-#include "adb_mdns.h"
 #include "adb_utils.h"
 #include "adb_wifi.h"
 #include "client/usb.h"
@@ -68,11 +67,9 @@ void adb_server_cleanup() {
     //   1. close_smartsockets, so that we don't get any new clients
     //   2. kick_all_transports, to avoid writing only part of a packet to a transport.
     //   3. usb_cleanup, to tear down the USB stack.
-    //   4. mdns_cleanup, to tear down mdns stack.
     close_smartsockets();
     kick_all_transports();
     usb_cleanup();
-    mdns_cleanup();
 }
 
 static void intentionally_leak() {
