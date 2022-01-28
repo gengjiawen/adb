72f35744d1b3afaf2e0399fb1b1a440e05e96cd1
Vince Harron <vharron@google.com>
2022-01-28 14:16:22 -0800

    Allow --one-device to be specified at any time.
    This prevents accidental startup of server in "all devices" mode.
    
    Bug: 209696568
    Test: Started adb server in one device mode with and without
    start-server.
    
    Change-Id: Iaa6892f3573cfd95c2f979614bbfefe53b3f2cd3

--- client/commandline.cpp
+++ client/commandline.cpp
@@ -95,9 +95,9 @@ static void help() {
         " -P                       port of adb server [default=5037]\n"
         " -L SOCKET                listen on given socket for adb server"
         " [default=tcp:localhost:5037]\n"
-        " --one-device SERIAL|USB  only allowed with 'start-server' or 'server nodaemon', server"
-        " will only connect to one USB device, specified by a serial number or USB device"
-        " address.\n"
+        " --one-device SERIAL|USB  will only connect to one USB device, specified by a serial"
+        " number or USB device (e.g. usb:2-6). Only used when starting server. Ignored"
+        " otherwise.\n"
         "\n"
         "general commands:\n"
         " devices [-l]             list connected devices (-l for long output)\n"
@@ -1678,12 +1678,6 @@ int adb_commandline(int argc, const char** argv) {
             LOG(FATAL) << "failed to allocate server socket specification";
         }
         server_socket_str = temp;
-    }
-
-    bool server_start =
-            is_daemon || is_server || (argc > 0 && strcmp(argv[0], "start-server") == 0);
-    if (one_device_str && !server_start) {
-        error_exit("--one-device is only allowed when starting a server.");
     }
 
     adb_set_one_device(one_device_str);
