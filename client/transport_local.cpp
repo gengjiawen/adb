ba5f064cdd307638aa5763160af3bf379d647ea8
Dmitry Ivanov <dmitry.ivanov@opensynergy.com>
2022-09-09 10:17:42 +0200

    adb: Preserve emulator serial on reconnect
    Emulator connection recovery logic ignores current console port number.
    In case of emulator with not standard console port number after
    connection loss and recovery cycle it will appear in the device list
    with wrong serial/console port number. Fix it by saving emulator's
    original console port number.
    
    Test: manual
    1) adb kill-server
    2) adb start-server
    
    3.a) Start emulator with non standard ports
    3.b) Or manually connect to running emulator
         adb connect emu:4444,6666
    
    4) Check list of devices:
    adb devices
    List of devices attached
    emulator-4444   device
    
    <---restart emulator--->
    
    5.a) ADB server without patch:
    adb devices
    List of devices attached
    emulator-6665   device
    
    5.b) ADB server with applied fix:
    adb devices
    List of devices attached
    emulator-4444   device
    
    Change-Id: I74fb5d1dbc322ad4c9b20ce6273a93f8ddddfbc8
    Signed-off-by: Dmitry Ivanov <dmitry.ivanov@opensynergy.com>

--- client/transport_local.cpp
+++ client/transport_local.cpp
@@ -178,6 +178,7 @@ static constexpr auto LOCAL_PORT_RETRY_INTERVAL = 1s;
 
 struct RetryPort {
     int port;
+    int console_port;
     uint32_t retry_count;
 };
 
@@ -210,8 +211,9 @@ static void client_socket_thread(std::string_view) {
         for (auto& port : ports) {
             VLOG(TRANSPORT) << "retry port " << port.port << ", last retry_count "
                             << port.retry_count;
-            if (local_connect(port.port)) {
-                VLOG(TRANSPORT) << "retry port " << port.port << " successfully";
+            std::string error;
+            if (local_connect_arbitrary_ports(port.console_port, port.port, &error) == 0) {
+                VLOG(TRANSPORT) << "retry port " << port.port << " successful";
                 continue;
             }
             if (--port.retry_count > 0) {
@@ -236,14 +238,15 @@ void local_init(const std::string& addr) {
 }
 
 struct EmulatorConnection : public FdConnection {
-    EmulatorConnection(unique_fd fd, int local_port)
-        : FdConnection(std::move(fd)), local_port_(local_port) {}
+    EmulatorConnection(unique_fd fd, int local_port, std::optional<int> console_port)
+        : FdConnection(std::move(fd)), local_port_(local_port), console_port_(console_port) {}
 
     ~EmulatorConnection() {
         VLOG(TRANSPORT) << "remote_close, local_port = " << local_port_;
         std::unique_lock<std::mutex> lock(retry_ports_lock);
         RetryPort port;
         port.port = local_port_;
+        port.console_port = console_port_.value_or(local_port_ - 1);
         port.retry_count = LOCAL_PORT_RETRY_COUNT;
         retry_ports.push_back(port);
         retry_ports_cond.notify_one();
@@ -256,6 +259,7 @@ struct EmulatorConnection : public FdConnection {
     }
 
     int local_port_;
+    std::optional<int> console_port_;
 };
 
 /* Only call this function if you already hold local_transports_lock. */
@@ -281,6 +285,14 @@ std::string getEmulatorSerialString(int console_port) {
     return android::base::StringPrintf("emulator-%d", console_port);
 }
 
+static std::optional<int> getEmulatorConsolePort(std::string serial) {
+    int console_port;
+    if (sscanf(serial.c_str(), "emulator-%d", &console_port) == 1) {
+        return console_port;
+    }
+    return std::nullopt;
+}
+
 int init_socket_transport(atransport* t, unique_fd fd, int adb_port, int local) {
     int fail = 0;
 
@@ -288,7 +300,8 @@ int init_socket_transport(atransport* t, unique_fd fd, int adb_port, int local) 
 
     // Emulator connection.
     if (local) {
-        auto emulator_connection = std::make_unique<EmulatorConnection>(std::move(fd), adb_port);
+        auto emulator_connection = std::make_unique<EmulatorConnection>(
+                std::move(fd), adb_port, getEmulatorConsolePort(t->serial));
         t->SetConnection(
                 std::make_unique<BlockingConnectionAdapter>(std::move(emulator_connection)));
         std::lock_guard<std::mutex> lock(local_transports_lock);
