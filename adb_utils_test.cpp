80b02442a9e547117ef4a2c4b33819cc802ce707
Shaju Mathew <shaju@google.com>
2023-07-05 17:01:32 -0700

    Addressing failing tests on Windows(get_host_socket_spec_port() and socket_spec_connect()) as well as assorted legacy failures on Mac.
    Bug: 290119691
    
    Test: Ran adbd_test on Windows 10 Enterprise, Mac M1 and Linux hosts
      - Windows: nativetest\adb_test>.\adb_test.exe
        Running main() from external/googletest/googletest/src/gtest_main.cc
        [==========] Running 76 tests from 16 test suites.
        <snip>
        [----------] 8 tests from AdbListenersTest (647 ms total)
        [----------] Global test environment tear-down
        [==========] 76 tests from 16 test suites ran. (153984 ms total)
        [  PASSED  ] 76 tests.
          YOU HAVE 8 DISABLED TESTS
      - Mac M1:
        $ out/host/darwin-x86/nativetest64/adb_test/adb_test
        <snip>
        [----------] Global test environment tear-down
        [==========] 82 tests from 17 test suites ran. (5898 ms total)
        [  PASSED  ] 82 tests.
      - Linux: aosp-master-with-phones$ out/host/linux-x86/nativetest64/adb_test/adb_test
        <snip>
        [----------] Global test environment tear-down
        [==========] 86 tests from 17 test suites ran. (15532 ms total)
        [  PASSED  ] 86 tests.
    
    Change-Id: I2989cf18b4aa77591852d2813a2ec1b274cd40cd

--- adb_utils_test.cpp
+++ adb_utils_test.cpp
@@ -53,9 +53,9 @@ TEST(adb_utils, directory_exists) {
   ASSERT_TRUE(directory_exists(profiles_dir));
 
   ASSERT_FALSE(directory_exists(subdir(profiles_dir, "does-not-exist")));
-#else
-  ASSERT_TRUE(directory_exists("/proc"));
-  ASSERT_FALSE(directory_exists("/proc/does-not-exist"));
+#elif ADB_LINUX
+    ASSERT_TRUE(directory_exists("/proc"));
+    ASSERT_FALSE(directory_exists("/proc/does-not-exist"));
 #endif
 }
 
@@ -71,13 +71,13 @@ TEST(adb_utils, directory_exists_win32_symlink_junction) {
   // C:\ProgramData. Symbolic links are rare on Windows and the user requires
   // a special permission (by default granted to Administrative users) to
   // create symbolic links.
-  EXPECT_FALSE(directory_exists(subdir(profiles_dir, "All Users")));
+  EXPECT_TRUE(directory_exists(subdir(profiles_dir, "All Users")));
 
   // On modern (English?) Windows, this is a directory junction to
   // C:\Users\Default. Junctions are used throughout user profile directories
   // for backwards compatibility and they don't require any special permissions
   // to create.
-  EXPECT_FALSE(directory_exists(subdir(profiles_dir, "Default User")));
+  EXPECT_TRUE(directory_exists(subdir(profiles_dir, "Default User")));
 }
 #endif
 
