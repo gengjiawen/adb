d3d3e2b247b3f07e5663d9184f31c4c229e5f0b7
Fabien Sanglard <sanglardf@google.com>
2023-09-27 21:31:41 +0000

    Unify usage of "\0jdwp-control"
    This is a companion CL to aosp/2764069 which aims at having a single
    definition of the abstract UDS control socket (vs previously having 3).
    
    Test: Already covered by CTS and adb tests.
    Bug: 302381118
    Change-Id: I16d0001fa46e04d6ed9131e0bb2d38b3b36cdca0

---
+++ libs/adbconnection/common.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "adbconnection/common.h"
+
+#include <sys/socket.h>
+
+#include <string>
+
+std::tuple<sockaddr_un, socklen_t> get_control_socket_addr() {
+  using namespace std::string_literals;
+  std::string kJdwpControlName = "\0jdwp-control"s;
+  assert(kJdwpControlName.size() <= sizeof(reinterpret_cast<sockaddr_un*>(0)->sun_path));
+
+  sockaddr_un addr = {};
+  addr.sun_family = AF_UNIX;
+  memcpy(addr.sun_path, kJdwpControlName.c_str(), kJdwpControlName.size());
+  socklen_t addrlen = offsetof(sockaddr_un, sun_path) + kJdwpControlName.size();
+
+  return {addr, addrlen};
+}
