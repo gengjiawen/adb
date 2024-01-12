9ca6c170aebade6edb8e4613f255c8fd8622f048
Shaju Mathew <shaju@google.com>
2024-01-12 07:15:07 +0000

    Treating libusb init failure(due to absent device mount/controller) as non-fatal. Missing /dev/bus/usb device mounts on cloud-hosted servers result in libusb initialization failure.
    Bug: 318975562
    
    Test: Treehugger (failure is reproduced only by a select few users).
    Change-Id: I944c72a5ddf582e68a70489ba539e837086151a4

--- client/usb_libusb.cpp
+++ client/usb_libusb.cpp
@@ -1028,8 +1028,12 @@ namespace libusb {
 void usb_init() {
     VLOG(USB) << "initializing libusb...";
     int rc = libusb_init(nullptr);
-    if (rc != 0) {
-        LOG(FATAL) << "failed to initialize libusb: " << libusb_error_name(rc);
+    if (rc != 0) {  // Cloud-hosted systems without a physical USB controller
+        // will not have /dev/bus/usb or /sys/dev/usb and typically
+        // they employ an alternate mode of hosting devices (e.g based on TCP)
+        LOG(WARNING) << "failed to initialize libusb (missing /dev/bus/usb entries?): "
+                     << libusb_error_name(rc);
+        return;
     }
 
     // Register the hotplug callback.
