cf0ddfe33ce969782521994b011ffcf651c3043e
Fabien Sanglard <sanglardf@google.com>
2024-03-05 21:33:44 +0000

    Appinfo: Make adb the app debug source of truth
    Design doc: go/adb_app_info
    
    Test: test_device.py
    Bug: 333161320
    
    Change-Id: Ie2d4cc1d355d1bc4f6009bf7252fcf6c9c05acdb

--- libs/adbconnection/include/adbconnection/client.h
+++ libs/adbconnection/include/adbconnection/client.h
@@ -51,6 +51,40 @@ struct AdbConnectionClientInfo {
 AdbConnectionClientContext* adbconnection_client_new(
     const AdbConnectionClientInfo* const* info_elems, size_t info_count);
 
+// Update the apex client with the new name of the process. Nothing is transferred to the server.
+// You need to call adbconnection_client_send_update to transmit the latest state to adbd.
+void adbconnection_client_set_current_process_name(const char* process_name);
+
+// Update the apex client when a package name is added to the current process. Nothing is
+// transferred to the server. You need to call adbconnection_client_send_update to transmit the
+// latest state to adbd.
+void adbconnection_client_add_application(const char* package_name);
+
+// Update the apex client when a package name is removed from the current process. Nothing is
+// transferred to the server. You need to call adbconnection_client_send_update to transmit the
+// latest state to adbd.
+void adbconnection_client_remove_application(const char* package_name);
+
+// Update the apex client when the app is waiting for debugger (or not). Nothing is
+// transferred to the server. You need to call adbconnection_client_send_update to transmit the
+// latest state to adbd.
+void adbconnection_client_set_waiting_for_debugger(bool waiting);
+
+// Update the apex client when app process uid is known. This is not the value from getuid() but
+// the UserID profile (e.g.: A device with a single user will have one UserID=0 but a device with
+// an additional work profile will have a second UserID). The origin of this value is the unix UID
+// massaged in Framework via PER_USER_RANGE and USER_SYSTEM. Nothing is transferred to the server.
+// You need to call adbconnection_client_send_update to transmit the latest state to adbd.
+void adbconnection_client_set_user_id(int uid);
+
+// Check if the client has something to send to the server (we don't want to be woken by a
+// writable socket if we have nothing to write). If it does, adbconnection_client_send_update
+// should be called.
+bool adbconnection_client_has_pending_update();
+
+// Write the latest appinfo state so adbd receives it.
+void adbconnection_client_send_update(AdbConnectionClientContext* ctx);
+
 void adbconnection_client_destroy(AdbConnectionClientContext* ctx);
 
 // Get an fd which can be polled upon to detect when a jdwp socket is available.
