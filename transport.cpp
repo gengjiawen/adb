b711ced2649249b0d13ecfe4fb65606cf7767256
Fabien Sanglard <sanglardf@google.com>
2024-07-03 13:58:52 -0700

    Refactor should_use_libusb
    The function used to be predicate. Now is it a prompt for the
    state of the USB backend.
    
    Test: NA
    Bug: NA
    Change-Id: Ib9be4ddb0282c9101c0362684d27b676047813eb

--- transport.cpp
+++ transport.cpp
@@ -731,7 +731,7 @@ void update_transports() {
 static bool usb_devices_start_detached() {
     static const char* env = getenv("ADB_LIBUSB_START_DETACHED");
     static bool result = env && strcmp("1", env) == 0;
-    return should_use_libusb() && result;
+    return is_libusb_enabled() && result;
 }
 #endif
 
@@ -1076,7 +1076,7 @@ bool atransport::Attach(std::string* error) {
     D("%s: attach", serial.c_str());
     fdevent_check_looper();
 
-    if (!should_use_libusb()) {
+    if (!is_libusb_enabled()) {
         *error = "attach/detach only implemented for libusb backend";
         return false;
     }
@@ -1103,7 +1103,7 @@ bool atransport::Detach(std::string* error) {
     D("%s: detach", serial.c_str());
     fdevent_check_looper();
 
-    if (!should_use_libusb()) {
+    if (!is_libusb_enabled()) {
         *error = "attach/detach only implemented for libusb backend";
         return false;
     }
